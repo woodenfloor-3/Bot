d06228a322eae335dab296f2b3f8fa21a04aa37a
woodenfloor-3 <127944520+woodenfloor-3@users.noreply.github.com>
2024-06-29 18:12:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '7171709688:AAHP4n_elhnMZxWK7HbCcsBqhFh_8VrLVqY'
+BOT_TOKEN = <Enter Telegram Bot Token here>
 SAUCENAO_API_URL = 'https://saucenao.com/search.php?'
-SAUCENAO_API_KEY = 'ab523b9c520c7a1ba4383ee0d15c6a7ec1242ca0'
+SAUCENAO_API_KEY = <Add Saucenao API key here>
